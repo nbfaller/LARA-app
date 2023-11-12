2fc80e24c59a1e9fca3dfdb69ca1f00e7be9240e
nbfaller <nbfaller@up.edu.ph>
2023-11-12 21:18:51 +0800

    Added modules.

---
+++ index.py
@@ -0,0 +1,46 @@
+from dash import dcc, html
+import dash_bootstrap_components
+import dash
+from dash.dependencies import Input, Output, State
+from dash.exceptions import PreventUpdate
+import webbrowser
+from app import app
+
+# Layout definition
+CONTENT_STYLE = {
+    "margin-top" : "4em",
+    "margin-left" : "1em",
+    "margin-right" : "1em",
+    "padding" : "1em 1em"
+}
+
+app.layout = html.Div(
+    [
+        dcc.Location(id = 'url', refresh = True),
+        html.Div(id = 'page-content', style = CONTENT_STYLE)
+    ]
+)
+
+@app.callback(
+    [Output('page-content', 'children')],
+    [Input('url', 'pathname')]
+)
+
+def displaypage(pathname):
+    ctx = dash.callback_context
+    if ctx.triggered:
+        eventid = ctx.triggered[0]['prop_id'].split('.')[0]
+        if eventid == 'url':
+            if pathname == '/' or pathname == '/home':
+                returnlayout = "home"
+            else:
+                returnlayout = 'error404'
+            return [returnlayout]
+        else:
+            raise PreventUpdate
+    else:
+        raise PreventUpdate
+
+if __name__ == '__main__':
+    webbrowser.open('http://127.0.0.1:8050/', new = 0, autoraise = True)
+    app.run_server(debug = False)
